004e78e1fb4b97976de77504f069e065cbb44fbc
panther2504 <ravichandran2504@gmail.com>
2023-09-04 16:02:12 +0530

    updated

---
+++ js/skills.js
@@ -0,0 +1,10 @@
+// let percentHtml = document.getElementById("percentHtml");
+// let html = 0;
+// setInterval(() => {
+//   if (html == 70) {
+//     clearInterval();
+//   } else {
+//     html += 1;
+//     percentHtml.innerHTML = html + "%";
+//   }
+// }, 28);
